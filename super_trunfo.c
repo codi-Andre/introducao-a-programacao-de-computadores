dede1c12e4118ab1f318d94fd1af4cd59c5ad08f
André Miranda <120103103+codi-Andre@users.noreply.github.com>
2025-04-25 13:55:31 +0000

    :sparkles: feat: conclui o nível intermediário do desafio super trunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,7 +6,7 @@ int main() {
     
     char estado1, estado2, codigo1[3], codigo2[3], cidade1[255], cidade2[255];
     int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
-    float area_em_km_quadrados1, area_em_km_quadrados2, pib1, pib2;
+    float area_em_km_quadrados1, area_em_km_quadrados2, pib1, pib2, densidade_populacional1, densidade_populacional2, pib_per_capita1, pib_per_capita2;
 
     // Cadastro das Cartas:
 
@@ -78,21 +78,32 @@ int main() {
     getchar();
     printf("\n");
 
+    // Calculos extras
+
+    densidade_populacional1 = (float) populacao1 / area_em_km_quadrados1;
+    densidade_populacional2 = (float) populacao2 / area_em_km_quadrados2;
+    pib_per_capita1 = (float) (pib1 * 1000000000) / populacao1;
+    pib_per_capita2 = (float) (pib2 * 1000000000) / populacao2;
+
     // Exibição dos Dados das Cartas:
 
     printf("Carta: %c%s\n", estado1, codigo1);
-    printf("Cidade: %s", cidade1);
+    printf("Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area_em_km_quadrados1);
     printf("PIB de %s: %.2f\n", cidade1, pib1);
     printf("Pontos turísticos: %d\n", pontos_turisticos1);
-
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional1);
+    printf("PIB per capita: %.2f reais\n", pib_per_capita1);
+    
     printf("\n\n\n");
     
     printf("Carta %c%s\n", estado2, codigo2);
-    printf("Cidade: %s", cidade2);
+    printf("Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area_em_km_quadrados2);
     printf("PIB de %s: %.2f\n", cidade2, pib2);
     printf("Pontos turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional2);
+    printf("PIB per capita: %.2f reais\n", pib_per_capita2);
 }
